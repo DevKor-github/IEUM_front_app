2689ed14eed22f5213a2fdbfbba883d718994cbb
anna <imannamj@gmail.com>
2024-09-04 18:00:34 +0900

    [Feat] 홈화면 보관함 및 장소 리스트 구현

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import {
   View,
   StyleSheet,
@@ -6,8 +6,10 @@ import {
   SafeAreaView,
   Pressable,
   Dimensions,
+  ScrollView,
+  Alert,
 } from 'react-native';
-import {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import {StackScreenProps} from '@react-navigation/stack';
 import {HomeStackParamList} from '../../types';
 import MainLogo from '../assets/main-logo-small.svg';
 import NotificationIcon from '../assets/notification-icon.svg';
@@ -15,8 +17,8 @@ import EmptyProfile from '../assets/empty-profile.svg';
 import EditIcon from '../assets/edit-icon.svg';
 import LinkInputIcon from '../assets/link-input-icon.svg';
 import LinkInputButton from '../assets/agreement-toggle-button.svg';
+import FolderSavedPlaceNum from '../assets/folder-saved-place-num.svg';
 import {API} from '../api/base';
-import {StackScreenProps} from '@react-navigation/stack';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import {useFocusEffect} from '@react-navigation/native';
 
@@ -31,107 +33,168 @@ interface LinkData {
   savedCollectionPlacesCount: number;
 }
 
-const dHeight = Dimensions.get('window').height;
+interface Folder {
+  id: number;
+  name: string;
+  placeCnt: number;
+  type: number;
+}
+
+interface Place {
+  id: number;
+  name: string;
+  simplifiedAddress: string;
+  mappedCategory: string;
+  imageUrl: string;
+}
+
 const dWidth = Dimensions.get('window').width;
 
 const HomeScreen = ({navigation}: HomeScreenProps) => {
   const [unviewedLinksCount, setUnviewedLinksCount] = useState<number>(0);
   const [unSavedPlacesCount, setUnSavedPlacesCount] = useState<number>(0);
+  const [savedPlaces, setSavedPlaces] = useState<Place[]>([]);
   const [username, setUserName] = useState('');
+  const [folders, setFolders] = useState<Folder[]>([]);
 
-  useEffect(() => {
-    async function getUserProfile() {
+  const [cursorId, setCursorId] = useState<number | null>(0);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const fetchSavedPlaces = useCallback(async () => {
+    if (isLoadingMore || !hasMoreData || cursorId === null) return;
+
+    try {
       const accessToken = await EncryptedStorage.getItem('accessToken');
-      const res = await API.get('/users/me/profile', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
+      setIsLoadingMore(true);
+
+      const response = await API.get('/folders/default/places-list', {
+        headers: {Authorization: `Bearer ${accessToken}`},
+        params: {
+          take: 10,
+          cursorId: cursorId || 0,
+          addressList: [],
+          categoryList: [],
         },
       });
-      if (res.status === 200) {
-        setUserName(res.data.response.nickname);
-      }
-    }
-    getUserProfile();
-  }, []);
 
-  useEffect(() => {
-    async function getFolder() {
+      const data = response.data.response.data;
+      const meta = response.data.response.meta;
+
+      setSavedPlaces(prev => [...prev, ...data]);
+      setCursorId(meta.hasNextPage ? meta.nextCursorId : null);
+      setHasMoreData(meta.hasNextPage);
+    } catch (error) {
+      console.error('Error fetching saved places:', error);
+      Alert.alert('Error', 'An error occurred while fetching saved places.');
+    } finally {
+      setIsLoadingMore(false);
+    }
+  }, [cursorId, isLoadingMore, hasMoreData]);
+
+  const fetchData = useCallback(async () => {
+    try {
       const accessToken = await EncryptedStorage.getItem('accessToken');
-      const res = await API.get('/folders', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      if (res.data.statusCode === 9001 || res.data.statusCode === 9003) {
-        console.log('SignUp Needed');
-        navigation.navigate('ServiceAgreement');
+
+      const [profileRes, folderRes, unviewedLinksRes, viewedLinksRes] =
+        await Promise.all([
+          API.get('/users/me/profile', {
+            headers: {Authorization: `Bearer ${accessToken}`},
+          }),
+          API.get('/folders', {
+            headers: {Authorization: `Bearer ${accessToken}`},
+          }),
+          API.get('/collections/unviewed', {
+            headers: {Authorization: `Bearer ${accessToken}`},
+            params: {cursorId: cursorId},
+          }),
+          API.get('/collections/viewed', {
+            headers: {Authorization: `Bearer ${accessToken}`},
+            params: {cursorId: cursorId},
+          }),
+        ]);
+
+      if (profileRes.status === 200) {
+        setUserName(profileRes.data.response.nickname);
       }
-    }
-    getFolder();
-  }, [navigation]);
 
-  useFocusEffect(() => {
-    async function fetchLinkData() {
-      try {
-        const accessToken = await EncryptedStorage.getItem('accessToken');
-        const unviewedResponse = await API.get<{response: {data: LinkData[]}}>(
-          '/collections/unviewed',
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-            params: {
-              cursorId: 0,
-            },
-          },
-        );
-        const viewedResponse = await API.get<{response: {data: LinkData[]}}>(
-          '/collections/viewed',
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-            params: {
-              cursorId: 0,
-            },
-          },
-        );
-
-        const unviewedLinks = unviewedResponse.data.response.data;
-        const viewedLinks = viewedResponse.data.response.data;
-
-        setUnviewedLinksCount(unviewedLinks.length);
-
-        const totalUnSavedPlaces = unviewedLinks.reduce(
-          (acc: number, link: LinkData) =>
-            acc +
-            (link.collectionPlacesCount - link.savedCollectionPlacesCount),
-          0,
-        );
-
-        const totalViewedUnSavedPlaces = viewedLinks.reduce(
-          (acc: number, link: LinkData) =>
-            acc +
-            (link.collectionPlacesCount - link.savedCollectionPlacesCount),
-          0,
-        );
-
-        setUnSavedPlacesCount(totalUnSavedPlaces + totalViewedUnSavedPlaces);
-      } catch (error) {
-        console.error('Error fetching link data:', error);
+      if (folderRes.status === 200) {
+        setFolders(folderRes.data.response.data);
       }
-    }
+      const unviewedLinks = unviewedLinksRes.data.response.data;
+      const viewedLinks = viewedLinksRes.data.response.data;
 
-    fetchLinkData();
-  });
+      setUnviewedLinksCount(unviewedLinks.length);
+
+      const totalUnViewedUnSavedPlaces = unviewedLinks.reduce(
+        (acc: number, link: LinkData) =>
+          acc + (link.collectionPlacesCount),
+        0,
+      );
+
+      const totalViewedUnSavedPlaces = viewedLinks.reduce(
+        (acc: number, link: LinkData) =>
+          acc + (link.collectionPlacesCount),
+        0,
+      );
+
+      setUnSavedPlacesCount(
+        totalUnViewedUnSavedPlaces + totalViewedUnSavedPlaces - savedPlaces.length,
+      );
+      fetchSavedPlaces();
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      Alert.alert('Error', 'An error occurred while fetching data.');
+    }
+  }, [fetchSavedPlaces]);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, [fetchData]),
+  );
+
+  const renderFolders = () => {
+    const folderItems = folders.slice(0, 4).map((folder, index) => (
+      <View
+        key={folder.id}
+        style={[styles.folderItem, {marginRight: index % 2 === 0 ? 8 : 0}]}>
+        <Text style={styles.folderName}>{folder.name}</Text>
+        <View style={styles.folderInfo}>
+          <FolderSavedPlaceNum />
+          <Text style={styles.folderPlaceCount}>
+            저장한 장소 · {folder.placeCnt}곳
+          </Text>
+        </View>
+      </View>
+    ));
+    return <View style={styles.foldersContainer}>{folderItems}</View>;
+  };
+
+  const renderPlaces = () => {
+    const placeItems = savedPlaces.slice(0, 4).map((place, index) => (
+      <View
+        key={place.id}
+        style={[styles.placeItem, {marginRight: index % 2 === 0 ? 8 : 0}]}>
+        <View style={styles.placeImage} />
+        <Text style={styles.placeName}>{place.name}</Text>
+        <Text style={styles.placeInfo}>
+          {place.simplifiedAddress} | {place.mappedCategory}
+        </Text>
+      </View>
+    ));
+
+    return <View style={styles.placesContainer}>{placeItems}</View>;
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <MainLogo />
-          <NotificationIcon />
-        </View>
+      <View style={styles.header}>
+        <MainLogo />
+        <NotificationIcon />
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View style={styles.profileSection}>
           <EmptyProfile />
           <Text style={styles.nicknameText}>{username} 님의 공간</Text>
@@ -154,12 +217,55 @@ const HomeScreen = ({navigation}: HomeScreenProps) => {
           </View>
           <LinkInputButton style={styles.linkInputButton} />
         </Pressable>
-        <Pressable
-          onPress={() => navigation.navigate('LinkInput')}
-          style={styles.addLinkButton}>
-          <Text style={styles.addLinkButtonText}>😝 링크 추가하기</Text>
-        </Pressable>
-      </View>
+
+        <View style={styles.sectionContainer}>
+          <Text style={styles.sectionTitle}>내 보관함 🚘</Text>
+          {renderFolders()}
+          <Pressable
+            style={styles.viewAllButton}
+            onPress={() =>
+              navigation.navigate('FolderList', {newFolder: null})
+            }>
+            <Text style={styles.viewAllButtonText}>전체보기</Text>
+          </Pressable>
+        </View>
+
+        <View style={{width: dWidth, height: 12, backgroundColor: '#F8F8F8'}} />
+
+        <View style={{marginTop: 30, width: dWidth - 48, alignItems: 'center'}}>
+          <Text
+            style={{
+              fontSize: 18,
+              fontWeight: '700',
+              alignSelf: 'flex-start',
+              marginBottom: 18,
+            }}>
+            내 장소 📍
+          </Text>
+          {renderPlaces()}
+          <Pressable
+            style={{
+              width: 172,
+              height: 40,
+              borderColor: '#D9D9D9',
+              borderWidth: 1,
+              borderRadius: 2,
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginTop: 25,
+              marginBottom: 25,
+            }}
+            onPress={() => navigation.navigate('PlaceList')}>
+            <Text style={{fontSize: 15, fontWeight: '600'}}>전체보기</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+
+      <Pressable
+        onPress={() => navigation.navigate('LinkInput')}
+        style={styles.addLinkButton}>
+        <Text style={styles.addLinkButtonText}>😝 링크 추가하기</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
@@ -173,6 +279,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
+  },
+  scrollViewContent: {
+    alignItems: 'center',
+    paddingBottom: 30,
   },
   header: {
     flexDirection: 'row',
@@ -240,6 +350,89 @@ const styles = StyleSheet.create({
     marginLeft: 'auto',
     width: 9,
     height: 14,
+  },
+  sectionContainer: {
+    width: dWidth - 48,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: '700',
+    alignSelf: 'flex-start',
+  },
+  foldersContainer: {
+    marginTop: 15,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  folderItem: {
+    width: (dWidth - 58) / 2,
+    height: 105,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+    marginRight: 8,
+  },
+  folderName: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFF',
+  },
+  folderInfo: {
+    flexDirection: 'row',
+    marginTop: 5,
+    alignItems: 'center',
+  },
+  folderPlaceCount: {
+    fontSize: 12.5,
+    fontWeight: '500',
+    color: '#FFF',
+    marginLeft: 7,
+  },
+  placesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  placeItem: {
+    width: (dWidth - 58) / 2,
+    marginBottom: 20,
+  },
+  placeImage: {
+    width: '100%',
+    height: 220,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 6,
+  },
+  placeName: {
+    marginTop: 9,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  placeInfo: {
+    fontSize: 12.5,
+    color: '#A4A4A4',
+    fontWeight: '500',
+  },
+  viewAllButton: {
+    width: 172,
+    height: 40,
+    borderColor: '#D9D9D9',
+    borderWidth: 1,
+    borderRadius: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 25,
+    marginBottom: 25,
+  },
+  viewAllButtonText: {
+    fontSize: 15,
+    fontWeight: '600',
   },
   addLinkButton: {
     position: 'absolute',
