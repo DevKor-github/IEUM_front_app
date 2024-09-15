1842ed9a7b30be86369b58a7c12b119610d540e6
jeongmin <jeongmin010324@gmail.com>
2024-09-15 14:58:57 +0900

    Feat : 시뮬레이터 에러 로그 제거

--- App.tsx
+++ App.tsx
@@ -2,8 +2,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigations/Stack';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {RecoilRoot} from 'recoil';
+import {useEffect} from 'react';
+import {LogBox} from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+  }, []);
   return (
     <GestureHandlerRootView style={{flex: 1}}>
       <NavigationContainer>
