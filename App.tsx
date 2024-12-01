e0cb4421a615aa3cde225ff706061a47cd8e5d4e
jeongmin <jeongmin010324@gmail.com>
2024-12-01 20:35:33 +0900

    feat : amplitude 추가

--- App.tsx
+++ App.tsx
@@ -4,9 +4,10 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {RecoilRoot} from 'recoil';
 import {useEffect} from 'react';
 import {LogBox} from 'react-native';
-
+import * as amplitude from '@amplitude/analytics-react-native';
 const App = () => {
   useEffect(() => {
+    amplitude.init('eb69b03aceb1ca5069e875c838302c96');
     LogBox.ignoreAllLogs(); // 앱에 뜨는 로그 제거
   }, []);
   return (
